29b39f1d30ee88716feece4be7bbcd6b152de313
kanteer <59344076+Kanteer@users.noreply.github.com>
2020-04-27 14:28:43 -0400

    Update index.js

--- index.js
+++ index.js
@@ -5,4 +5,4 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.login('NzA0MzU1NjkyODE0Nzk0ODE0.XqcF8A.TIYGEXCLR3cpPlMjeqvGPYZ7Xw0');
+client.login('NzA0MzU1NjkyODE0Nzk0ODE0.XqciTg.bRERmJoosVpATluXTGoPd0ztzwI');
