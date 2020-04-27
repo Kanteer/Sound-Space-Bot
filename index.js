0c8f7f0733bc96b12e834ae393c2b318010c520f
kanteer <59344076+Kanteer@users.noreply.github.com>
2020-04-27 14:28:03 -0400

    Update index.js

--- index.js
+++ index.js
@@ -5,10 +5,4 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
-  }
-});
-
 client.login('NzA0MzU1NjkyODE0Nzk0ODE0.XqcF8A.TIYGEXCLR3cpPlMjeqvGPYZ7Xw0');
