46af95b59a54ba29e9a4320241a721483c1b79c0
kanteer <59344076+Kanteer@users.noreply.github.com>
2020-04-27 14:27:39 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
 
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('Pong!');
+  }
+});
+
+client.login('NzA0MzU1NjkyODE0Nzk0ODE0.XqcF8A.TIYGEXCLR3cpPlMjeqvGPYZ7Xw0');
